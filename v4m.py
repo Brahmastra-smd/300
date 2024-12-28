b33c4d657e0b4efc086911e62b61e5ab81bc66eb
Brahmastra-smd <bgmitk1@gmail.com>
2024-12-28 20:18:17 +0530

    Update v4m.py

--- v4m.py
+++ v4m.py
@@ -110,7 +110,7 @@ async def start_asyncio_loop():
         await asyncio.sleep(1)
 
 async def run_attack_command_async(message_id, chat_id, target_ip, target_port, duration):
-    process = await asyncio.create_subprocess_shell(f"./bgmi {target_ip} {target_port} {duration} ")
+    process = await asyncio.create_subprocess_shell(f"./bgmi {target_ip} {target_port} {duration} 1000")
     await process.communicate()
 
     # After the attack finishes, update the message
