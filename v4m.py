dc17aa478d5958ee741978088127c3c6c3c27eef
Brahmastra-smd <bgmitk1@gmail.com>
2024-12-28 20:25:02 +0530

    Update v4m.py

--- v4m.py
+++ v4m.py
@@ -16,7 +16,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '6997279925:AAG1G9f1LKFjd07EOprIYbzgAQoldA8SId8'
+TOKEN = '6997279925:AAF7_lBFut__CcMul3X7zdzGFwjgkkdKbzI'
 MONGO_URI = 'mongodb+srv://botplays:botplays@botplays.0xflp.mongodb.net/?retryWrites=true&w=majority&appName=Botplays'
 FORWARD_CHANNEL_ID = -1002165028046
 CHANNEL_ID = -1002165028046
